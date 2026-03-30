e2b8bdf8b636ff190db9081c0dd62351f869be4c
Harishlohar0 <harishlohar77777@gmail.com>
2026-03-30 22:25:26 +0530

    update

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: process.env.EMAIL_USER,
       to,
       subject,
       html,
